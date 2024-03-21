9df016687cddac53aa187e6c463136d8fcbfffb9
KenRemen <kerem9811@oslomet.no>
2024-03-21 22:19:04 +0100

    continuing project 12 - smoothing it up

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -7,35 +7,23 @@
 // and reduce unnecessary backend calls.
 $('document').ready(() => {
 
-    $('#orderform').submit(event => {
-        event.preventDefault();
-
-        const orderform = $('#orderform')[0];
-        const isOK = orderform.checkValidity();
-        if (isOK) {
-            void addtoTickets();
-        } else {
-            alert("Oh no!");
-            // console.log(displayErrorMessages(orderform))
-            // void displayErrorMessages(orderform);
-
-        }
-    })
-
     $('#autofyll').click(autoFillInfo);
     $('#slettalt').click(deleteTicketsNew);
+
+    $('#orderform').submit(event => {
+        event.preventDefault();
+        checkForm().then(data => {
+            if (data) {
+                console.log("Validering av skjema er " + data);
+                void addtoTickets();
+            } else {
+                alert("Du må sjekke input :(");
+                console.log("Validering av skjema er " + data);
+            }
+        })
+    })
     void refreshTicketlist();
 })
-
-/*
-async function displayErrorMessages(form) {
-    const invalidFields = $(form).find(":invalid");
-    invalidFields.each(function () {
-        const errorMessage = $(this).prop('validationMessage');
-        $(this).siblings('.error-message').text(errorMessage);
-    });
-}
-*/
 
 async function addtoTickets() {
     // Lage billett-objekt med tilhørende verdier
@@ -52,27 +40,6 @@ async function addtoTickets() {
 
     // Send billett til backend
     await $.post("/tickets/add", ticket);
-
-    // Lage array
-    let ticketArray = [await $.get("/tickets/list")];
-
-    // Logge array til konsoll
-    for (let i = 0; i < ticketArray.length; i++) {
-        console.log(ticketArray[i]);
-    }
-    let arrayToString = JSON.stringify(ticketArray);
-    console.log(arrayToString);
-
-    /*
-    * const myArray= [4,7,2,5,6];
-console.log(myArray);
-const myString=myArray.toString();
-console.log(myString);
-    * */
-
-    /*for (let i = 0; i < cars.length; i++) {
-text += cars[i] + "<br>";
-}*/
 
     // Legg billett til tabell
     await refreshTicketlist();
@@ -93,16 +60,137 @@ async function refreshTicketlist() {
 
     // Sende billetter til tabell
     let ut = null;
+    let nr = 1;
     ut += "<tr>" +
-        "<th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th>" +
+        "<th>Nr</th><th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th>" +
         "</tr>";
     // Så blir hver billett printet i en tabell gjennom en for-løkke
     for (let i of tickets) {
         ut += "<tr>";
-        ut += "<td>" + i.film + "</td><td>" + i.amount + "</td><td>" + i.firstname + "</td><td>" + i.lastname + "</td><td>" + i.tel + "</td><td>" + i.email + "</td>";
+        ut += "<td>" + nr + "<td>" + i.film + "</td><td>" + i.amount + "</td><td>" + i.firstname + "</td><td>" + i.lastname + "</td><td>" + i.tel + "</td><td>" + i.email + "</td>";
         ut += "</tr>";
+        nr++;
     }
     $("#liste").html(ut);
+}
+
+// Sjekking av regex
+const regEx = {
+    // Det må velges en film
+    film: /[^ ]/,
+    // Antall må være 1-99
+    amount: /^[1-9][0-9]?$/,
+    // Kan ikke stå tom, har også required tag i html. (https://www.kalzumeus.com/2010/06/17/falsehoods-programmers-believe-about-names/)
+    name: /^(?!\s*$).+/i,
+    // Aksepterer nummer med 8 siffer som begynner med 2-7 eller 9, eller et internasjonalt nummer med minimum 6 siffer. (https://nkom.no/telefoni-og-telefonnummer/telefonnummer-og-den-norske-nummerplan/alle-nummerserier-for-norske-telefonnumre)
+    tel: /^(?:[2-7,9]\d{7}|(?:\+|00)\d{6,})$/,
+    // Aksepterer "normale" epost-adresser (https://www.regular-expressions.info/email.html)
+    email: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+}
+
+async function checkFilm() {
+    let filmOK = false;
+    if ($("#film").val() !== "") {
+        filmOK = true;
+        $("#filmPopup").css("visibility", "hidden");
+        $("#filmPopup").css("z-index", 1);
+    } else if ($("#film").val() === "") {
+        $("#filmPopup").css("visibility", "visible");
+        $("#filmPopup").css("z-index", 10000);
+    }
+    return filmOK;
+}
+
+async function checkAmount() {
+    let antallRegex = regEx.amount.test($("#antall").val());
+    let antallValidity = document.getElementById('antall').checkValidity();
+    let bothOK = false;
+    if (!antallRegex || !antallValidity) {
+        $("#antallPopup").css("visibility", "visible");
+        $("#antallPopup").css("z-index", 10000);
+    } else {
+        $("#antallPopup").css("visibility", "hidden");
+        $("#antallPopup").css("z-index", 1);
+        bothOK = true;
+    }
+    return bothOK;
+}
+
+// Funksjon som sjekker fornavn-input
+async function checkFirstname() {
+    let firstnameRegex = regEx.name.test($("#fornavn").val());
+    let firstnameValidity = document.getElementById('fornavn').checkValidity();
+    let bothOK = false;
+    if (!firstnameRegex || !firstnameValidity) {
+        $("#fornavnPopup").css("visibility", "visible");
+        $("#fornavnPopup").css("z-index", 10000);
+    } else if (firstnameRegex && firstnameValidity) {
+        $("#fornavnPopup").css("visibility", "hidden");
+        $("#fornavnPopup").css("z-index", 1);
+        bothOK = true;
+    }
+    return bothOK;
+}
+
+// Funksjon som sjekker etternavn-input
+async function checkLastname() {
+    let lastnameRegex = regEx.name.test($("#etternavn").val());
+    let lastnameValidity = document.getElementById('etternavn').checkValidity();
+    let bothOK = false;
+    if (!lastnameRegex || !lastnameValidity) {
+        $("#etternavnPopup").css("visibility", "visible");
+        $("#etternavnPopup").css("z-index", 10000);
+    } else if (lastnameRegex && lastnameValidity) {
+        $("#etternavnPopup").css("visibility", "hidden");
+        $("#etternavnPopup").css("z-index", 1);
+        bothOK = true;
+    }
+    return bothOK;
+}
+
+// Sjekker telefon-input
+async function checkTelephone() {
+    let telRegex = regEx.tel.test($("#tlf").val());
+    let telValidity = document.getElementById('tlf').checkValidity();
+    let bothOK = false;
+    if (!telRegex || !telValidity) {
+        $("#tlfPopup").css("visibility", "visible");
+        $("#tlfPopup").css("z-index", 10000);
+    } else if (telRegex && telValidity) {
+        $("#tlfPopup").css("visibility", "hidden");
+        $("#tlfPopup").css("z-index", 1);
+        bothOK = true;
+    }
+    return bothOK;
+}
+
+// Sjekker epost-input
+async function checkEmail() {
+    let emailRegex = regEx.email.test($("#epost").val());
+    let emailValidity = document.getElementById('epost').checkValidity();
+    let bothOK = false;
+    if (emailRegex && emailValidity) {
+        $("#epostPopup").css("visibility", "hidden");
+        $("#epostPopup").css("z-index", 1);
+        bothOK = true;
+    } else if (!emailRegex || !emailValidity) {
+        $("#epostPopup").css("visibility", "visible");
+        $("#epostPopup").css("z-index", 10000);
+    }
+    return bothOK;
+}
+
+async function checkForm() {
+    let validationFunctionsArray = [
+        await checkFilm(),
+        await checkAmount(),
+        await checkFirstname(),
+        await checkLastname(),
+        await checkTelephone(),
+        await checkEmail(),
+    ]
+    // Hvis alle verdier kommer tilbake som sanne blir total-verdien også sann
+    return !validationFunctionsArray.includes(false);
 }
 
 // Fyll ut info knapp, for å slippe å skrive inn hver eneste gang :) ----------------------------------------
@@ -114,7 +202,8 @@ async function autoFillInfo() {
     const randomEtternavn = etternavnArray[Math.floor(Math.random() * etternavnArray.length)];
     const randomEpost = epostArray[Math.floor(Math.random() * epostArray.length)];
     $("#film").val(Math.floor(Math.random() * (9) + 1));
-    // Velger det minste tallet fra et array med 10 tilfeldig genererte tall, for realisme.
+
+    // Velger det minste tallet fra et array med 10 tilfeldig genererte tall, for litt billettkjøp-realisme.
     $("#antall").val(Math.ceil(Math.min(...Array.from({length: 10}, Math.random)) * 100));
     $("#fornavn").val(randomFornavn);
     $("#etternavn").val(randomEtternavn);
